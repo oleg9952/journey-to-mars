a7cc319237cf2b3de793c8059cedce19061b2ff7
Oleh Parkhomenko <oleh_parkhomenko@epam.com>
2020-03-11 12:49:33 +0000

    Gallery && ESLint / Husky

--- src/pug/components/navigation/navigation.js
+++ src/pug/components/navigation/navigation.js
@@ -1,28 +1,28 @@
 export const navigation = () => {
-    // ----- DOM Elements -----
-    const agencies = document.querySelector('.nav__column-agencies');
-    const infomodal = document.querySelector('.infomodal');
-    const closeBtn = document.querySelector('.infomodal__close-btn');
+	// ----- DOM Elements -----
+	const agencies = document.querySelector('.nav__column-agencies');
+	const infomodal = document.querySelector('.infomodal');
+	const closeBtn = document.querySelector('.infomodal__close-btn');
 
-    agencies.addEventListener('click', e => {
-        document.body.style.overflow = 'hidden';
-        if(e.target.classList[1] === 'agency--one') {
-            infomodal.classList.add('show--nasa');
-        } else if(e.target.classList[1] === 'agency--two') {
-            infomodal.classList.add('show--ukr');
-        } else if(e.target.classList[1] === 'agency--three') {
-            infomodal.classList.add('show--spacex');
-        }
-    })
+	agencies.addEventListener('click', (e) => {
+		document.body.style.overflow = 'hidden';
+		if (e.target.classList[1] === 'agency--one') {
+			infomodal.classList.add('show--nasa');
+		} else if (e.target.classList[1] === 'agency--two') {
+			infomodal.classList.add('show--ukr');
+		} else if (e.target.classList[1] === 'agency--three') {
+			infomodal.classList.add('show--spacex');
+		}
+	});
 
-    const triggers = [
-        'show--nasa',
-        'show--ukr',
-        'show--spacex'
-    ]
+	const triggers = [
+		'show--nasa',
+		'show--ukr',
+		'show--spacex',
+	];
 
-    closeBtn.addEventListener('click', () => {
-        document.body.style.overflow = 'visible';
-        triggers.forEach(trigger => infomodal.classList.remove(trigger))
-    })
-}
+	closeBtn.addEventListener('click', () => {
+		document.body.style.overflow = 'visible';
+		triggers.forEach((trigger) => infomodal.classList.remove(trigger));
+	});
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,92 @@
+/* eslint-disable semi */
+const webpack = require('webpack');
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const PUG_TO_HTML_PATS = [
-    { output: 'index.html', input: './pug/pages/home/index.pug' },
-    { output: 'booking.html', input: './pug/pages/booking/booking.pug' },
-    { output: 'gallery.html', input: './pug/pages/gallery/gallery.pug' }
-]
+	{ output: 'index.html', input: './pug/pages/home/index.pug' },
+	{ output: 'booking.html', input: './pug/pages/booking/booking.pug' },
+	{ output: 'gallery.html', input: './pug/pages/gallery/gallery.pug' }
+];
  
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    mode: 'development',
-    entry: {
-        main: './javascript/index.js'
-    },
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        port: 4200
-    },
-    plugins: [
-        ...PUG_TO_HTML_PATS.map(item => {
-            return new HTMLWebpackPlugin({
-                filename: item.output,
-                template: item.input
-            })
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    { loader: MiniCssExtractPlugin.loader },
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                loader: 'file-loader',
-                options: {
-                  name: '[name].[ext]'
-                },
-            },
-            {
-                test: /\.pug$/,
-                loader: 'pug-loader',
-                options: { pretty: true }
-            }
-        ]
-    }
+	devtool: '#@cheap-module-eval-source-map',
+	context: path.resolve(__dirname, 'src'),
+	mode: 'development',
+	entry: {
+		main: './javascript/index.js'
+	},
+	output: {
+		filename: '[name].[contenthash].js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	devServer: {
+		port: 4200
+	},
+	plugins: [
+		...PUG_TO_HTML_PATS.map(item => {
+			return new HTMLWebpackPlugin({
+				filename: item.output,
+				template: item.input
+			});
+		}),
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: '[name].[contenthash].css'
+		}),
+		new webpack.SourceMapDevToolPlugin({
+			filename: '[file].map'
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					{ loader: MiniCssExtractPlugin.loader },
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: true,
+							config: { path: './postcss.config.js' }
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true
+						}
+					}
+				]
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				loader: 'file-loader',
+				options: {
+				  name: '[name].[ext]'
+				},
+			},
+			{
+				test: /\.pug$/,
+				loader: 'pug-loader',
+				options: { pretty: true }
+			}, 
+			{
+				test: /\.js$/,
+				loader: 'eslint-loader',
+				exclude: /node_modules/,
+				options: {
+					sourceMap: true
+				}
+			}
+		]
+	}
 }
